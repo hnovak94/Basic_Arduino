a86c403d2480dec8eb7606600aac4f268d2fe790
Harriet Novak <hnovak94@ccs.local>
2019-10-07 12:09:10 -0400

    Assignment Description in README.md

--- New_ping/NewPing.ino
+++ New_ping/NewPing.ino
@@ -2,12 +2,12 @@
 #include <NewPing.h>
 
 
-int trigPin = 11; //This makes the pin a variable
+int trigPin = 11; 
 int echoPin = 12; 
 int maxDist = 200; 
  
 long duration;
-int cm; // says that the variable distance is an integer
+int cm; 
  
 Servo myServo;
 int servoPin = 9;
@@ -16,35 +16,41 @@ NewPing myHCSR04(trigPin, echoPin, maxDist);
  
  
 void setup(){
-	pinMode(trigPin, OUTPUT); //sets the trigPin as an OUTPUT
-	pinMode(echoPin, INPUT); //sets the echoPin as an INPUT
-	Serial.begin(9600); //Starts the serial monitor
+	pinMode(trigPin, OUTPUT); 
+	pinMode(echoPin, INPUT); 
+	Serial.begin(9600);
 	myServo.attach(servoPin);
 }
  
  
  
-void loop(){ //This makes the servo move or not move
+void loop() { 
+
 	cm = myHCSR04.ping_cm();
-	if (cm != 0 && cm <150){ //This makes it so the code doesn't print zeros or numbers above 150
-		if(cm < 10){
-			moveServo(); //This function makes the servo move 180
+
+	if (cm != 0 && cm <= 45) {
+			if(cm < 10) {
+				moveServo();
+			
 		}
-		else{
-		stopServo();//This function makes the servo stop 
-		}
-	Serial.println(cm);
-	}
-	delay(10);
-} 
- 
- 
-void moveServo(){
-	myServo.write(180); //The servo will move 180
-}
+		else {
+		stopServo();
+		} 
+	
+		Serial.println(cm); 
+		delay(10);
+	 }
+	 delay(10); }
+	 
 
  
+
+ void moveServo() {
+ 	myServo.write(180);
+ }
+
+ void stopServo() {
+ 	myServo.write(90);
+ }
  
-void stopServo(){
-	myServo.write(90); //The servo will stop
-}
+
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 NewPing, HelloFunction, not so basic arduino assignments. 
 
 My Arduino Assignments (Not so basic arduino)
+
+NewPing- Servo moves until distance reaches a certain point
+
+HelloFunction- Servo changes speed depending on distance
